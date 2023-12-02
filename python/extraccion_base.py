49d5578eb99f1cc8a9df6264dad3a5be4b8b510e
pepe54aguilar <pep343000@gmail.com>
2023-12-02 11:10:28 +0100

    changes

--- python/extraccion_base.py
+++ python/extraccion_base.py
@@ -10,19 +10,32 @@ def valenbisi_api(url: str):
     datos = respuesta.json()
     if(status_code == 200):
         estaciones_totales:int = datos["total_count"]
+        paradas = []
         for i, estacion in enumerate(range(100)):
+            parada=[]
             clase = datos['results'][i]
             nombre_estacion: str = clase["address"]
             numero:int = clase["number"]
             abierta:bool = clase["open"]
             totales:int = clase["total"]
-            bici_disponibles:int = clase["available"]
-            hueco_disponible:int = clase["free"]
+            bici_disponibles:list = []
+            hueco_disponible:list = []
             ticket:bool = clase["ticket"]
-            fecha_hora:str = clase["updated_at"]
+            fecha_hora:list = []
             coordenadas:list = clase["geo_shape"]["geometry"]["coordinates"]
+            parada.append(nombre_estacion)
+            parada.append(numero)
+            parada.append(abierta)
+            parada.append(totales)
+            parada.append(bici_disponibles)
+            parada.append(hueco_disponible)
+            parada.append(ticket)
+            parada.append(fecha_hora)
+            parada.append(coordenadas)
+            paradas.append(parada)
+        return(paradas)
     else:
         print(f"Error: {status_code}")
-
+    
 valenbisi_api(URL)
 
--- python/extraccion_continua.py
+++ python/extraccion_continua.py
@@ -10,12 +10,20 @@ def cambios_valenbisi(url: str):
     datos = respuesta.json()
     if(status_code == 200):
         estaciones_totales:int = datos["total_count"]
+        actualizaciones = []
         for i, estacion in enumerate(range(100)):
+            actualizacion = []
             clase = datos['results'][i]
+            nombre_estacion: str = clase["address"]
             bici_disponibles:int = clase["available"]
             hueco_disponible:int = clase["free"]
             fecha_hora:str = clase["updated_at"]
+            actualizacion.append(nombre_estacion)
+            actualizacion.append(bici_disponibles)
+            actualizacion.append(hueco_disponible)
+            actualizacion.append(fecha_hora)
+        return(actualizaciones.append(actualizacion))
     else:
         print(f"Error: {status_code}")
 
-cambios_valenbisi(URL)
+print(cambios_valenbisi(URL))

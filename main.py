fa5b41c6ab5801948464b981e4c5e993cd6edef8
pepe54aguilar <pep343000@gmail.com>
2023-12-15 15:30:20 +0100

    jupyter

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ URL:str = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/v
 intervalo_tiempo = 600
 
 while True:
-    valenbisi_api(URL)
+    try:
+        valenbisi_api(URL)
+    except Exception as e:
+        print("Error: {e}")
     time.sleep(intervalo_tiempo)
+    
 
 
 
